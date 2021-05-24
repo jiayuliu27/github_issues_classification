88d00cd936c66f1e10fb88c927c177bf216bf778
Tiffany <jiayuliu@umich.edu>
2021-05-24 16:04:13 -0400

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying github issues based on its title and description',
+    author='Tiffany Liu',
+    license='MIT',
+)
